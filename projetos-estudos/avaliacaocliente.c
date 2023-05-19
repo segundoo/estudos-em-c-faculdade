42896795617534fa71c37afe0a5e8088b83889e7
Edilson Segundo <edilsonsegundoo@outlook.com>
2023-05-18 21:44:53 -0300

    ATVD1
    Sistema de avaliação de clientes.

---
+++ projetos-estudos/avaliacaocliente.c
@@ -0,0 +1,23 @@
+//bibliotecas
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+    int qtdRuim, qtdRegular, qtdbom, qtdOtimo, opcao;
+
+    tdRuim = 0;
+    qtdRegular = 0;
+    qtdbom = 0;
+    qtdOtimo = 0;
+
+    do 
+    {
+        printf ("Avalie o nosso atendimento. Digite uma opção:")
+        printf ("1 - RUIM");
+        printf
+    }
+
+
+    return 0;
+}
