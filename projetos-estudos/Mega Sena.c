ce64b128599a9fd3e4a9b4923acb66d47f040251
Edilson Segundo <edilsonsegundoo@outlook.com>
2023-06-27 22:50:15 -0300

    Correção
    Correção de uma parte do codigo, restando apenas corrigir não sortear o mesmo número 2 vezes

--- projetos-estudos/Mega Sena.c
+++ projetos-estudos/Mega Sena.c
@@ -2,39 +2,39 @@
 #include <stdlib.h>
 #include <time.h>
 
-int numeroRandomicoNaoRepetido(int sorteioMega[6]) {
-	int numeroSorteado;
-	
-        srand(time(NULL));
-	    numeroSorteado = rand() % 60 + 1;
-
-	return numeroSorteado;	
+int numeroRandomicoNaoRepetido(int sorteioMega[], int index) {
+    int numeroSorteado;
+    int repetido = 0;
+    do {
+        numeroSorteado = rand() % 60 + 1; // Sorteia um número de 1 a 60
+        
+        // Verifica se o número já foi sorteado anteriormente
+        for (int i = 0; i < index; i++) {
+            if (sorteioMega[i] == numeroSorteado) {
+                repetido = 1;
+                break;
+            }
+        }
+        
+        // Repete o sorteio se o número já foi sorteado antes
+    } while (repetido = 0);
+    
+    return numeroSorteado;	
 }
 
-int main()
-{
-    int sorteioMega[6], x = 0, j = 0, igual;
-
-    do
-    {
-        sorteioMega[x] = numeroRandomicoNaoRepetido(sorteioMega);
-        for (j = 0; j < x; j++) 
-        {
-            if (sorteioMega[x] == sorteioMega[j])
-            {
-                igual = 1;
-            }    
-        }
-        if (igual == 0)
-        {
-            x++;
-        }
-        printf("Numeros sorteados de 1 a 60 da Mega Sena\n");
-        for (x = 0; x < 6; x++)
-        {
-            printf("%i ,", sorteioMega[x]);
-        }   
-    } while (x < 6);
+int main() {
+    int sorteioMega[6];
+    srand(time(NULL));
     
-	return 0;
+    // Gera os números aleatórios sem repetição
+    for (int i = 0; i < 6; i++) {
+        sorteioMega[i] = numeroRandomicoNaoRepetido(sorteioMega, i);
+    }
+    
+    printf("Números sorteados de 1 a 60 na Mega Sena:\n");
+    for (int i = 0; i < 6; i++) {
+        printf("%d ", sorteioMega[i]);
+    }
+    
+    return 0;
 }
