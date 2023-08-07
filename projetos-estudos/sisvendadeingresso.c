b2f26a331a591cb988534b828dc41b3e6ef099d3
Edilson Segundo <edilsonsegundoo@outlook.com>
2023-08-07 13:48:49 -0300

    .

--- projetos-estudos/sisvendadeingresso.c
+++ projetos-estudos/sisvendadeingresso.c
@@ -1,166 +1,180 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
+#include <stdbool.h>
+#include <string.h>
 
-struct dadoscliente
+typedef struct
 {
-   char nome [100];
-   char cpf [15];
-   int anonasc;
-};
+  char nome[100];
+  char cpf[15];
+  int anonasc;
+} dadoscliente;
 
-struct dadoscliente dados[999];
+dadoscliente dados[999];
 int opcao;
-int contIngresso;
+int contIngresso = 0;
 
-void venderIngresso() {
+void venderIngresso()
+{
+	setlocale(LC_ALL, "");
+  while (true)
+  {
+    printf("Nome do cliente: ");
+    fflush(stdin);
+    fgets(dados[contIngresso].nome, 100, stdin);
+    printf("CPF do cliente: ");
+    fflush(stdin);
+    fgets(dados[contIngresso].cpf, 10, stdin);
+    printf("Ano de nascimento do cliente: ");
+    fflush(stdin);
+    scanf("%d", &dados[contIngresso].anonasc);
+
+    contIngresso++;
+
+    printf("Deseja continuar cadastrando?\n");
+    printf("[1]SIM [2] Menu principal\n");
+    fflush(stdin);
+    scanf("%d", &opcao);
+    if (opcao == 2)
+    {
+      menu();
+    }
+    else
+    {
+      venderIngresso();
+    }
+  }
+}
+
+void listarIngresso()
+{
+	setlocale(LC_ALL, "");
+  int i;
+  printf("---------------------------------------\n");
+  printf("-----LISTAGEM DE INGRESSOS VENDIDOS----\n");
+  printf("---------------------------------------\n");
+  printf("");
+  printf("");
+  printf("");
+  printf("");
+  i = 0;
+  do
+  {
+    printf("---------------------------------------\n");
+    printf("              INGRESSO %d              \n", i + 1);
+    printf("---------------------------------------\n");
+    printf("Nome: %s\n", dados[i].nome);                 //[erro] não pecorre o vetor
+    printf("CPF: %s\n", dados[i].cpf);                   //[erro] não pecorre o vetor
+    printf("Ano de nascimento: %d\n", dados[i].anonasc); //[erro] não pecorre o vetor
+    printf("---------------------------------------\n");
+    printf("                                       \n");
+    printf("---------------------------------------\n");
+    i++;
+
+  } while (i < contIngresso);
+
+  printf("Deseja ver a lista de ingressos vendidos novamente?\n");
+  printf("[1]SIM [2] Menu principal\n");
+  fflush(stdin);
+  scanf("%d", &opcao);
+  if (opcao == 2)
+  {
+    menu();
+  }
+  else
+  {
+    listarIngresso();
+  }
+}
+
+void validarIngresso()
+{
+	setlocale(LC_ALL, "");
     int i = 0;
-    do
-    {   
-        printf("Nome do cliente: ");
-        fflush(stdin);
-        fgets(dados[i].nome, 100, stdin);
-        printf("CPF do cliente: ");
-        fflush(stdin);
-        fgets(dados[i].cpf, 10, stdin);
-        printf("Ano de nascimento do cliente: ");
-        fflush(stdin);
-        scanf("%d", &dados[i].anonasc);
-
-        printf("Deseja continuar cadastrando?\n");
-        printf("[1]SIM [2] Menu principal\n");
-        fflush(stdin);
-        scanf("%d", &opcao);
-        if (opcao == 2)
-        {
-            menu();
-        } else {
-            venderIngresso();
-        }
-
-        i++;
-        contIngresso++;
-        
-    } while (i <= contIngresso);
+    char validar[15];
+    setlocale(LC_ALL, "");
     
-}
+    printf("-----------------------\n");
+    printf("VALIDAÇÃO DE INGRESSO  \n");
+    printf("-----------------------\n");
 
-void listarIngresso() {
-    int i;
-    printf("---------------------------------------\n");
-    printf("-----LISTAGEM DE INGRESSOS VENDIDOS----\n");
-    printf("---------------------------------------\n");
-    printf("");
-    printf("");
-    printf("");
-    printf("");
-    i = 0;
-    do {
-        printf("---------------------------------------\n");
-        printf("              INGRESSO %d              \n", i + 1);
-        printf("---------------------------------------\n");
-        printf("Nome: %s\n", dados[i].nome);//[erro] não pecorre o vetor 
-        printf("CPF: %s\n", dados[i].cpf);//[erro] não pecorre o vetor
-        printf("Ano de nascimento: %d\n", dados[i].anonasc);//[erro] não pecorre o vetor
-        printf("---------------------------------------\n");
-        printf("                                       \n");
-        printf("---------------------------------------\n");
-        
-        printf("Deseja ver a lista de ingressos vendidos novamente?\n");
-        printf("[1]SIM [2] Menu principal\n");
-        fflush(stdin);
-        scanf("%d", &opcao);
-        if (opcao == 2)
-        {
-            menu();
-        } else {
-            listarIngresso();
-        }
-        i++;
-        
-    } while (i <= contIngresso);
-    
-}
-
-void validarIngresso() {
-    int i;
-    char validar[999];
-    char vCpf;
     do
     {
-        validar[i] = dados[i].cpf;
-        i++;
-    } while (i <= contIngresso);
-    
-    printf("-----------------------");
-    printf("VALIDAÇÃO DE INGRESSO  ");
-    printf("-----------------------");
+        printf("Digite o CPF para validar o ingresso: \n");
+        fflush(stdin);
+        fgets(validar, 15, stdin);
 
-    printf("Digite o CPF para validar o ingresso: \n");
-    fgets(vCpf, 11, stdin);
-
-    do {
-        if (vCpf == validar[i])
+        if (strcmp(validar, dados[i].cpf) != 0)
         {
-           printf("Ingresso validado!"); 
-        } else 
+            printf("Ingresso inválido!\n");
+        } 
+        else 
         {
-            printf("[ERRO] Ingresso invalido!");
+            printf("Ingresso válido!\n");
         }
-    } while (i < 999);
+        
+        i++;
+    } while (i < contIngresso);
 
     printf("Deseja fazer a validação de ingresso novamente?\n");
-        printf("[1]SIM [2] Menu principal\n");
-        fflush(stdin);
-        scanf("%d", &opcao);
-        if (opcao == 2)
-        {
-            menu();
-        } else {
-            validarIngresso();
-        }
-
-}
-
-void menu() {
-    setlocale( LC_ALL, "");
-    
-    int selecao; //variavel de seleção de opção no menu
-
-    //menu
-    printf("Escolha uma opção\n");
-    printf("1. Vender ingresso\n");
-    printf("2. Listar ingressos vendidos\n");
-    printf("3. Validar ingressos\n");
-    printf("0. Sair\n");
+    printf("[1]SIM [2] Menu principal\n");
     fflush(stdin);
-    scanf("%d", &selecao);
-    
-    switch (selecao)
+    scanf("%d", &opcao);
+    if (opcao == 2)
     {
-    case 1:
-        system("cls");
-        venderIngresso();
-        break;
-    case 2:
-        system("cls");
-        listarIngresso();
-        break;
-    case 3:
-        system("cls");
+        menu();
+    }
+    else
+    {
         validarIngresso();
-        break;
-    case 0:
-        printf("Saindo...");
-        exit(0);
-        break;
-    default:
-        printf("[ERRO] Opção invalida!");
-        break;
     }
 }
 
-int main() {
-    menu();
-    return 0;
+
+void menu()
+{
+  setlocale(LC_ALL, "");
+
+  int selecao; // variavel de seleção de opção no menu
+
+  // menu
+  printf("Escolha uma opção\n");
+  printf("1. Vender ingresso\n");
+  printf("2. Listar ingressos vendidos\n");
+  printf("3. Validar ingressos\n");
+  printf("0. Sair\n");
+  fflush(stdin);
+  scanf("%d", &selecao);
+
+  switch (selecao)
+  {
+  case 1:
+    system("cls");
+    venderIngresso();
+    break;
+  case 2:
+    system("cls");
+    listarIngresso();
+    break;
+  case 3:
+    system("cls");
+    validarIngresso();
+    break;
+  case 0:
+    printf("Saindo...");
+    exit(0);
+    break;
+  default:
+    printf("[ERRO] Opção invalida!");
+    break;
+  }
+}
+
+int main()
+{
+	printf ("VENDA DE INGRESSOS");
+	system("cls");
+	menu();
+	return 0;
 }
